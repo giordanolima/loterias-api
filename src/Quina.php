0ba750ddda1f81a1581d550af88927d96c50ec03
George Pacheco <georgecpacheco@gmail.com>
2019-04-10 15:17:54 -0300

    Ajuste na função dos Ganhadores da Quina
    Faltava os dados do terno e da duque.

--- src/Quina.php
+++ src/Quina.php
@@ -7,11 +7,13 @@ class Quina extends LoteriasApi {
     public function getGanhadores() {
         $retorno = [];
         if($this->json && array_key_exists("ganhadores", $this->json))
-            $retorno["sena"] = $this->json["ganhadores"];
-        if($this->json && array_key_exists("ganhadores_quina", $this->json))
-            $retorno["quina"] = $this->json["ganhadores_quina"];
+            $retorno["quina"] = $this->json["ganhadores"];
         if($this->json && array_key_exists("ganhadores_quadra", $this->json))
             $retorno["quadra"] = $this->json["ganhadores_quadra"];
+        if($this->json && array_key_exists("ganhadores_terno", $this->json))
+            $retorno["terno"] = $this->json["ganhadores_terno"];
+        if($this->json && array_key_exists("qt_ganhador_duque", $this->json))
+            $retorno["duque"] = $this->json["qt_ganhador_duque"];
 
         return $retorno;
         
